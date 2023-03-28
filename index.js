e305eb4f25067d516437ec7ac57d2a14fe231c4a
denisQa322 <denis.isakov.99@gmail.com>
2023-03-28 23:51:17 +0600

    competed slider on modal

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
+let sliderCount = 0;
 
-    let sliderCount = 0;
-
-const modalController = ({modal, openSlider, closeSlider, time = 500}) =>{
+const modalController = ({modal, openSlider, closeSlider, time = 500}) => {
     const openElements = document.querySelectorAll(openSlider)
     const modalElement = document.querySelector(modal);
-    const gallery = document.querySelector('.gallery');
-    const images = gallery.getElementsByTagName('img');
-    const sliderWrapper = document.querySelector('.slider-wrapper');
-
-    
-    
 
     modalElement.style.cssText = `
     display:flex;
@@ -17,92 +10,98 @@ const modalController = ({modal, openSlider, closeSlider, time = 500}) =>{
     opacity:0;
     transition: opacity ${time}ms ease-in-out;
     `
-    const closeModal = event =>{
+    const closeModal = event => {
         const target = event.target;
-        
-        if(target === modalElement ||
-            (closeSlider && target.closest(closeSlider))||
+
+        if (target === modalElement ||
+            (closeSlider && target.closest(closeSlider)) ||
             event.code === 'Escape'
-            ){
-                modalElement.style.opacity = 0;
-                
-                setTimeout(()=>{
-                    modalElement.style.visibility = 'hidden';
-                    sliderWrapper.style.transform = 'translateX(0px)';
-                    sliderCount = 0;
-                    console.log(sliderCount);
-                },time)
-                
-                window.removeEventListener('keydown', closeModal);
-            }
-            
+        ) {
+            modalElement.style.opacity = 0;
+
+            setTimeout(() => {
+                modalElement.style.visibility = 'hidden';
+                sliderCount = 0;
+                console.log(sliderCount);
+            }, time)
+
+            window.removeEventListener('keydown', closeModal);
         }
-        
-        function openModal(){
-            modalElement.style.visibility = 'visible';
-            modalElement.style.opacity = 1;
+
+    }
+
+    function openModal(id) {
+        return () => {
+            initSlider(id);
             window.addEventListener('keydown', closeModal)
-        }
-        
-        
-
-        openElements.forEach(img=>{
-            for(index = 0; index <  images.length; index++){
-                images[index].addEventListener('click',openModal);
-            }
-        })
-
-        
+        };
+    }
 
 
-    modalElement.addEventListener('click',closeModal);
+    openElements.forEach((img, id) => {
+        console.log('id', id);
+        img.addEventListener('click', openModal(id));
+    })
+
+    modalElement.addEventListener('click', closeModal);
 }
 
 modalController({
-    modal:'.modal',
-    openSlider:'.gallery-image',
-    closeSlider:'.slider-close',
+    modal: '.modal',
+    openSlider: '.gallery-image',
+    closeSlider: '.slider-close',
 })
-const Slider = () =>{
 
+initSlider = (id) => {
+    const modalElement = document.querySelector('.modal');
+    const sliderElement = document.querySelector('.slider');
+    const sliderWrapper = document.querySelector('.slider-wrapper');
+    let sliderWidth = sliderElement.offsetWidth - 30;
+    sliderCount = id;
+    sliderWrapper.style.transform = `translateX(${-id * sliderWidth}px)`;
+    console.log(sliderCount);
+    modalElement.style.visibility = 'visible';
+    modalElement.style.opacity = 1;
+}
+const Slider = () => {
     const sliderElement = document.querySelector('.slider');
     const sliderItem = document.querySelectorAll('.slider-item');
     const sliderWrapper = document.querySelector('.slider-wrapper');
     const leftArrow = document.querySelector('.slider-left');
     const rightArrow = document.querySelector('.slider-right');
-    
-    
+
     let sliderWidth = sliderElement.offsetWidth - 30;
-    
-    
-    function showSLide(){
+
+
+    function showSLide() {
         sliderItem.forEach(item => item.style.width = sliderWidth + 'px');
         rollSlider();
     };
     showSLide();
-    
-    function rollSlider(){
+
+    function rollSlider() {
         sliderWrapper.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
     }
-    function slideToLeft(){
+
+    function slideToLeft() {
         sliderCount--;
-        if(sliderCount < 0){
+        if (sliderCount < 0) {
             sliderCount = sliderItem.length - 1;
         }
         rollSlider();
     }
 
-    function slideToRight(){
+    function slideToRight() {
         sliderCount++;
-        if(sliderCount >= sliderItem.length){
+        if (sliderCount >= sliderItem.length) {
             sliderCount = 0;
         }
         rollSlider();
 
     }
-    
+
     leftArrow.addEventListener('click', slideToLeft)
-    rightArrow.addEventListener('click',slideToRight)
+    rightArrow.addEventListener('click', slideToRight)
 }
 
 Slider()
